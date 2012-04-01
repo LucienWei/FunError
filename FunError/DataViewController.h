3e6bbfcbad3307fe7f23ad488b5e029e34fe701a
Luicien Wei <lucienwei@hotmail.com>
2012-04-01 17:22:13 +0800

    Revert "Revert "Initial Commit""
    This reverts commit 022ad47b90469bbd0ad8c9d5856196aad8f977b7.

---
+++ FunError/DataViewController.h
@@ -0,0 +1,16 @@
+//
+//  DataViewController.h
+//  FunError
+//
+//  Created by Lucien Wei on 3/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
